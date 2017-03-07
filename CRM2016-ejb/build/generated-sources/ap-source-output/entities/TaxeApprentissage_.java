f695591d5b6862d6fd6760ab1665d73d2140e9a8
Julian Bellaïche <julianbellaiche@gmail.com>
2017-03-07 12:39:33 +0100

    Ajout du fichier .gitignore

--- CRM2016-ejb/build/generated-sources/ap-source-output/entities/TaxeApprentissage_.java
+++ CRM2016-ejb/build/generated-sources/ap-source-output/entities/TaxeApprentissage_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-07T11:54:07")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-07T12:25:13")
 @StaticMetamodel(TaxeApprentissage.class)
 public class TaxeApprentissage_ { 
 
